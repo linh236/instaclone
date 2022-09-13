844b76ca3a55e4402fbfef71c26615df36f541fa
oLinhNguyen <nguyenquang.linh@access-company.com>
2022-09-13 09:18:48 +0700

    [Main] fix select file on mobile and add sub-domain ngrok on development

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,21 +8,22 @@ import * as bootstrap from "bootstrap"
 window.bootstrap = bootstrap
 window.$ = window.jQuery = jQuery;
 
-$(".attachment-btn").click(function() {
-  let labelId = $(this).attr("for") ;
-  $(`#${labelId}`).change(function() {
-    let filename = $(this)[0].files;
-    $(".filename").html(filename[0].name);
+$(document).ready(function() {
+  $(".attachment-btn").click(function() {
+    let labelId = $(this).attr("for") ;
+    $(`#${labelId}`).change(function() {
+      let filename = $(this)[0].files;
+      $(".filename").html(filename[0].name);
+    })
   })
-})
-
-$(".comment-document").click(function() {
-  let labelId = $(this).attr("for") ;
-  let id = $(this).data("id");
-  $(`#${labelId}`).change(function() {
-    let filename = $(this)[0].files;
-    $(`.filename-document-${id}`).html(filename[0].name);
+  
+  $(".comment-document").click(function() {
+    let labelId = $(this).attr("for") ;
+    let id = $(this).data("id");
+    $(`#${labelId}`).change(function() {
+      let filename = $(this)[0].files;
+      $(`.filename-document-${id}`).html(filename[0].name);
+    })
   })
-})
-
+})  
 
